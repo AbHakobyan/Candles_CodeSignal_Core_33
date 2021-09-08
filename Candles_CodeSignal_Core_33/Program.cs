a0a8f8b87bcf47bd8230dfaa81948b3714934bdf
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-08 16:51:14 +0400

    Добавьте файлы проекта.

---
+++ Candles_CodeSignal_Core_33/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Candles_CodeSignal_Core_33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int candles = 6;
+            int make = 4;
+            int res = Candles(candles, make);
+            Console.WriteLine(res);
+        }
+
+        static int Candles(int candles, int makeNew)
+        {
+            int r = 0;
+            while (candles > 0)
+            {
+                candles--;
+                r++;
+                if (r % makeNew == 0)
+                    candles++;
+            }
+            return r;
+        }
+
+    }
+}
